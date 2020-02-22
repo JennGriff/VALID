31a1d6b13982c1d1e8e1cb022567013a0fa18067
JennGriff <jgriffiths0101@gmail.com>
2020-02-21 16:12:56 -0800

    initial Branching page for types of excuses

--- VALID/VALID/urls.py
+++ VALID/VALID/urls.py
@@ -1,4 +1,4 @@
-"""VALID URL Configuration
+"""Valid URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from excuseTypes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('emergency', views.emergency, name='emergency'),
+    path('scheduled', views.scheduled, name='scheduled'),
 ]
--- VALID/excuseTypes/views.py
+++ VALID/excuseTypes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'excuseTypes/home.html')
+
+def emergency(request):
+    return render(request, 'excuseTypes/emergency.html')
+
+def scheduled(request):
+    return render(request, 'excuseTypes/scheduled.html')
